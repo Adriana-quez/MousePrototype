d7572f80d357a3fe7666bcae44ee8d5cac244b94
Adriana Vasquez <99279815+Adriana-quez@users.noreply.github.com>
2025-10-15 01:03:55 -0400

    Edited player and added evidence

--- MousePrototype/Assets/Scripts/Player/PlayerMovement.cs
+++ MousePrototype/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 15;
     public float jump = 10;
     public Camera playerCamera;
-    private Rigidbody rigidBody;
+    private Rigidbody rb;
     private Vector3 moveDir = Vector3.zero;
     [SerializeField] private float friction = 10;
     [SerializeField] private float gravity = 3;
@@ -19,29 +19,32 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         float xMove = Input.GetAxisRaw("Horizontal");
         float yMove = Input.GetAxisRaw("Vertical");
         moveDir = ((transform.forward * yMove) + (transform.right * xMove)).normalized;
 
         if (Input.GetKeyDown(KeyCode.Space) && IsGround()) {
-            rigidBody.AddForce(transform.up * jump, ForceMode.Impulse);
+            rb.AddForce(transform.up * jump, ForceMode.Impulse);
         }
     }
 
-    void FixedUpdate() {
-        rigidBody.AddForce(moveDir.normalized * moveSpeed/10, ForceMode.Impulse);
+    void FixedUpdate() 
+    {
+        rb.AddForce(moveDir.normalized * moveSpeed/10, ForceMode.Impulse);
 
-        rigidBody.linearVelocity = new Vector3(rigidBody.linearVelocity.x * (100-friction)/100,
-        rigidBody.linearVelocity.y - gravity/10, rigidBody.linearVelocity.z * (100-friction)/100);
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x * (100-friction)/100,
+        rb.linearVelocity.y - gravity/10, rb.linearVelocity.z * (100-friction)/100);
     }
 
-    private bool IsGround() {
+    private bool IsGround() 
+    {
         return Physics.Raycast(transform.position, Vector3.down, offset + .15f);
     }
 }
